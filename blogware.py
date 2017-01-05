96de472bed4289360414fe497097bc1ec20cc20e
izrik <izrik@izrik.com>
2017-01-05 10:12:09 -0600

    Remove flask-bloccing and sqlalchemy.

--- blogware.py
+++ blogware.py
@@ -5,10 +5,8 @@ import random
 from os import environ
 
 from flask import Flask, render_template_string, redirect, render_template
-from sqlalchemy import create_engine, MetaData
 from flask_login import UserMixin, LoginManager, \
     login_user, logout_user, AnonymousUserMixin
-from flask_blogging import SQLAStorage, BloggingEngine
 
 
 class Config(object):
@@ -16,8 +14,6 @@ class Config(object):
     PORT = environ.get('BLOGWARE_PORT', 1177)
     DEBUG = environ.get('BLOGWARE_DEBUG', False)
     DB_URI = environ.get('BLOGWARE_DB_URI', 'sqlite:////tmp/blog.db')
-    URL_PREFIX = environ.get('BLOGWARE_URL_PREFIX', '')  # e.g. '/blog'
-    DISQUS_SITENAME = environ.get('BLOGWARE_DISQUS_SITENAME', '')
     SITENAME = environ.get('BLOGWARE_SITENAME', 'Site Name')
     SITEURL = environ.get('BLOGWARE_SITEURL', 'http://localhost:1177')
 
@@ -32,10 +28,6 @@ if __name__ == "__main__":
                         default=Config.DEBUG)
     parser.add_argument('--db-uri', type=str, action='store',
                         default=Config.DB_URI)
-    parser.add_argument('--url-prefix', type=str,
-                        default=Config.URL_PREFIX, help='')
-    parser.add_argument('--disqus-sitename', type=str,
-                        default=Config.DISQUS_SITENAME, help='')
     parser.add_argument('--sitename', type=str,
                         default=Config.SITENAME, help='')
     parser.add_argument('--siteurl', type=str,
@@ -55,8 +47,6 @@ if __name__ == "__main__":
     Config.PORT = args.port
     Config.DEBUG = args.debug
     Config.DB_URI = args.db_uri
-    Config.URL_PREFIX = args.url_prefix
-    Config.DISQUS_SITENAME = args.disqus_sitename
     Config.SITENAME = args.sitename
     Config.SITEURL = args.siteurl
 
@@ -64,19 +54,10 @@ if __name__ == "__main__":
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = Config.SECRET_KEY  # for WTF-forms and login
-app.config["BLOGGING_URL_PREFIX"] = Config.URL_PREFIX
-app.config["BLOGGING_DISQUS_SITENAME"] = Config.DISQUS_SITENAME
-app.config["BLOGGING_SITENAME"] = Config.SITENAME
-app.config["BLOGGING_SITEURL"] = Config.SITEURL
 app.config['SQLALCHEMY_DATABASE_URI'] = Config.DB_URI
 
 # extensions
-engine = create_engine(Config.DB_URI)
-meta = MetaData()
-sql_storage = SQLAStorage(engine, metadata=meta)
-blog_engine = BloggingEngine(app, sql_storage)
 login_manager = LoginManager(app)
-meta.create_all(bind=engine)
 
 
 # user class for providing authentication
@@ -99,15 +80,13 @@ class Guest(AnonymousUserMixin):
 
 
 @login_manager.user_loader
-@blog_engine.user_loader
 def load_user(user_id):
     return User("izrik", "izrik@izrik.com")
 
 
-if Config.URL_PREFIX and Config.URL_PREFIX != '/':
-    @app.route("/")
-    def index():
-        return render_template("index.html", config=blog_engine.config)
+@app.route("/")
+def index():
+    return render_template("index.html", config=Config)
 
 
 @app.route("/login")
@@ -125,10 +104,7 @@ def logout():
 
 if __name__ == "__main__":
 
-    print('Url prefix: {}'.format(app.config["BLOGGING_URL_PREFIX"]))
-    print('Site name: {}'.format(app.config["BLOGGING_SITENAME"]))
-    print('Site url: {}'.format(app.config["BLOGGING_SITEURL"]))
-    print('Disqus site name: {}'.format(
-        app.config["BLOGGING_DISQUS_SITENAME"]))
+    print('Site name: {}'.format(Config.SITENAME))
+    print('Site url: {}'.format(Config.SITEURL))
 
     app.run(debug=Config.DEBUG, port=Config.PORT, use_reloader=Config.DEBUG)
