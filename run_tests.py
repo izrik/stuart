3f3bb40f0913e6ddd57b8c0bfa94aecb4a3b5dda
izrik <izrik@izrik.com>
2017-02-10 16:10:13 -0600

    Create initial test runner script.

---
+++ run_tests.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import unittest
+import argparse
+import logging
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--print-log', action='store_true',
+                        help='Print the log.')
+    args = parser.parse_args()
+
+    if args.print_log:
+        logging.basicConfig(level=logging.DEBUG,
+                            format=('%(asctime)s %(levelname)s:%(name)s:'
+                                    '%(funcName)s:'
+                                    '%(filename)s(%(lineno)d):'
+                                    '%(threadName)s(%(thread)d):%(message)s'))
+
+    unittest.main(argv=[''])
+
+if __name__ == '__main__':
+    run()
