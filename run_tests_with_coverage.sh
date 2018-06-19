eff8cc6ec845ac27d8adb53d2035f9c3168acd99
izrik <izrik@izrik.com>
2018-06-19 11:37:09 -0500

    Run linters after tests.

--- run_tests_with_coverage.sh
+++ run_tests_with_coverage.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 coverage run --source=stuart ./run_tests.py "$@" && \
-    coverage html
+    coverage html && \
+    flake8 stuart.py run_tests.py && \
+    shellcheck run_tests_with_coverage.sh && \
+    markdownlint README.md && \
+    csslint static/stuart.css && \
+    safety check && \
+    echo Success
